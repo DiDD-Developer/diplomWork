601e664b1af2a16934f300702805c87480c9e400
DiDD-Developer <dimamaslov2005@vk.com>
2025-01-29 13:17:07 +0500

    add fix for style

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="mt-5">
     <nav class="container navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('/') }}"><img src="{{ asset('images/logo.png') }}" width="250px" alt="логотип сайта"></a>
+            <a class="navbar-brand" href="{{ route('/') }}"><img src="{{ asset('images/logo.png') }}" width="240px" alt="логотип сайта"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
